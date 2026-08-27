ab2f2c990996913dbf0a83c5a4524eb376bc26fc
Felipe gomes <felipewebdesignpro@gmail.com>
2026-08-27 14:28:36 -0300

    feat: add hero

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header"
+import { Hero } from "./components/Hero"
+
+function App() {
+  return (
+    <>
+    <main className="py-10">
+      <Header />
+
+      <Hero />
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import Logo from '@/assets/images/logo.png'
+import IconCart from '@/assets/images/icon cart.png'
+import IconUser from '@/assets/images/icon user.png'
+import IconAbout from '@/assets/images/icon about.png'
+
+export const Header = () => {
+    return (
+        <div className="relative">
+            <header className="fixed top-0 left-0 right-0 z-10 mx-10 py-10">
+                <div className="bg-header max-w-[1320px] mx-auto flex justify-between items-center py-5 px-7 rounded-2xl mt-5">
+                    <img src={Logo} alt="Logo Syntaxwear" className='w-32 md:w-36' />
+                    <nav className='hidden md:block'>
+                        <ul className='flex gap-10' >
+                            <li><a href="#">Masculino</a></li>
+                            <li><a href="#">Feminino</a></li>
+                            <li><a href="#">Outlet</a></li>
+                        </ul>
+                    </nav>
+
+                    <nav>
+                        <ul className='flex gap-4 md:gap-10'>
+                            <li className='hidden md:block'><a href="#">Nossas Lojas </a></li>
+                            <li className='hidden md:block'><a href="#">Sobre</a></li>
+                            <li><a href="#"> <img src={IconUser} alt="Icon User"/> </a></li>
+                            <li><a href="#"> <img src={IconAbout} alt="Icon About"/> </a></li>
+                            <li><a href="#"> <img src={IconCart} alt="Icon Cart"/>  </a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </div>
+    )
+}
