a12530de58032841c3b9e5fefa3ddb2e29a183c8
Felipe gomes <felipewebdesignpro@gmail.com>
2026-09-08 10:40:49 -0300

    feat:application route configuration

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,25 @@ import { Gallery } from "./components/Gallery"
 import { Header } from "./components/Header"
 import { Hero } from "./components/Hero"
 
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./router-tree-gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 function App() {
-  return (
+  return <RouterProvider router={router} />
+}
+
+export default App;
+
+
+/* (
     <>
       <main>
         <Header />
@@ -16,7 +32,4 @@ function App() {
       </main>
       <Footer />
     </>
-  )
-}
-
-export default App
+  ) */
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,16 +2,20 @@ import Logo from '@/assets/images/logo.png'
 import IconCart from '@/assets/images/icon cart.png'
 import IconUser from '@/assets/images/icon user.png'
 import IconAbout from '@/assets/images/icon about.png'
+import { Link } from '@tanstack/react-router'
 
 export const Header = () => {
     return (
         <div className="relative">
             <header className="fixed top-10 left-0 right-0 z-10 mx-10 mt-5">
-                <div className="bg-white max-w-[1320px] mx-auto flex justify-between items-center py-3 px-7 rounded-2xl">
+                <div className="bg-white text-black max-w-[1320px] mx-auto flex justify-between items-center py-3 px-7 rounded-2xl">
                     <img src={Logo} alt="Logo Syntaxwear" className='w-32 md:w-36' />
                     <nav className='hidden md:block'>
                         <ul className='flex gap-10' >
-                            <li><a href="#">Masculino</a></li>
+                            <li>
+                                
+                                <a href="#">Masculino</a>
+                                </li>
                             <li><a href="#">Feminino</a></li>
                             <li><a href="#">Outlet</a></li>
                         </ul>
@@ -21,7 +25,10 @@ export const Header = () => {
                         <ul className='flex gap-4 md:gap-10'>
                             <li className='hidden md:block'><a href="#">Nossas Lojas </a></li>
                             <li className='hidden md:block'><a href="#">Sobre</a></li>
-                            <li><a href="#"> <img src={IconUser} alt="Icon User"/> </a></li>
+                            <li>
+                            
+                            <Link to="/sign-in"><img src={IconUser} alt="Icon User"/></Link>
+                            </li>
                             <li><a href="#"> <img src={IconAbout} alt="Icon About"/> </a></li>
                             <li><a href="#"> <img src={IconCart} alt="Icon Cart"/>  </a></li>
                         </ul>
